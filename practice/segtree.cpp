dd693134ae1887a9341317497f1a65c6f54b9995
yonsweng <yonsweng@gmail.com>
2020-11-11 12:22:50 +0900

    Update segtree

--- practice/segtree.cpp
+++ practice/segtree.cpp
@@ -3,37 +3,39 @@
 using namespace std;
 
 template<typename T> class SegTree {
-    int MIN, MAX;
-    vector<T> node;
-    
+    // Segment tree가 구하고자 하는 값에 따라 변경
+    const T BOUND = 0;
     T f(T a, T b) { return a + b; }
 
-    T query(int node_num, int node_s, int node_e, int req_s, int req_e) {
-        if (node_s > req_e || node_e < req_s)
-            return 0;
+    int MIN, MAX;
+    vector<T> node;
+
+    T query(int k, int node_s, int node_e, int req_s, int req_e) {
+        if (req_e < node_s || node_e < req_s)
+            return BOUND;
         if (req_s <= node_s && node_e <= req_e)
-            return node[node_num];
+            return node[k];
         int node_m = (node_s + node_e) / 2;
-        T left = query(node_num * 2, node_s, node_m, req_s, req_e);
-        T right = query(node_num * 2 + 1, node_m + 1, node_e, req_s, req_e);
+        T left = query(k * 2, node_s, node_m, req_s, req_e);
+        T right = query(k * 2 + 1, node_m + 1, node_e, req_s, req_e);
         return f(left, right);
     }
-    void update(int node_num, int node_s, int node_e, int req_i, T value) {
+    void update(int k, int node_s, int node_e, int req_i, T value) {
         if (req_i < node_s || node_e < req_i)
             return;
-        if(node_s == node_e) {
-            node[node_num] = value;
+        if (node_s == node_e) {
+            node[k] = value;
             return;
         }
         int node_m = (node_s + node_e) / 2;
-        update(node_num * 2, node_s, node_m, req_i, value);
-        update(node_num * 2 + 1, node_m + 1, node_e, req_i, value);
-        node[node_num] = f(node[node_num * 2], node[node_num * 2 + 1]);
+        update(k * 2, node_s, node_m, req_i, value);
+        update(k * 2 + 1, node_m + 1, node_e, req_i, value);
+        node[k] = f(node[k * 2], node[k * 2 + 1]);
     }
 public:
     SegTree(int MIN, int MAX) : MIN(MIN), MAX(MAX) { node.resize(4 * (MAX - MIN)); }
     T query(int start, int end) { return query(1, MIN, MAX, start, end); }
-    void update(int i, T add) { update(1, MIN, MAX, i, add); }
+    void update(int i, T value) { update(1, MIN, MAX, i, value); }
 };
 
 int main() {
