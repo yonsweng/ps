89190d62e62a68e08b61f46264fda54fc8bae03e
yonsweng <yonsweng@gmail.com>
2020-11-11 12:39:11 +0900

    Edit segment tree

--- boj/10999.cpp
+++ boj/10999.cpp
@@ -6,46 +6,46 @@ template<typename T> class SegTree {
     int MIN, MAX;
     vector<T> node, lazy;
 
-    T init(int node_num, int node_s, int node_e, const vector<T>& data) {
+    T init(int k, int node_s, int node_e, const vector<T>& data) {
         if (node_s == node_e)
-            return node[node_num] = data[node_s];
+            return node[k] = data[node_s];
         int node_m = (node_s + node_e) / 2;
-        T left = init(node_num * 2, node_s, node_m, data);
-        T right = init(node_num * 2 + 1, node_m + 1, node_e, data);
-        return node[node_num] = left + right;
+        T left = init(k * 2, node_s, node_m, data);
+        T right = init(k * 2 + 1, node_m + 1, node_e, data);
+        return node[k] = left + right;
     }
-    void prop(int node_num, int node_s, int node_e) {
-        node[node_num] += lazy[node_num] * (node_e - node_s + 1);
+    void prop(int k, int node_s, int node_e) {
+        node[k] += lazy[k] * (node_e - node_s + 1);
         if (node_s != node_e) {
-            lazy[node_num * 2] += lazy[node_num];
-            lazy[node_num * 2 + 1] += lazy[node_num];
+            lazy[k * 2] += lazy[k];
+            lazy[k * 2 + 1] += lazy[k];
         }
-        lazy[node_num] = 0;
+        lazy[k] = 0;
     }
-    T query(int node_num, int node_s, int node_e, int req_s, int req_e) {
-        prop(node_num, node_s, node_e);
+    T query(int k, int node_s, int node_e, int req_s, int req_e) {
+        prop(k, node_s, node_e);
         if (req_e < node_s || node_e < req_s)
             return 0;
         if (req_s <= node_s && node_e <= req_e)
-            return node[node_num];
+            return node[k];
         int node_m = (node_s + node_e) / 2;
-        T left = query(node_num * 2, node_s, node_m, req_s, req_e);
-        T right = query(node_num * 2 + 1, node_m + 1, node_e, req_s, req_e);
+        T left = query(k * 2, node_s, node_m, req_s, req_e);
+        T right = query(k * 2 + 1, node_m + 1, node_e, req_s, req_e);
         return left + right;
     }
-    void update(int node_num, int node_s, int node_e, int req_s, int req_e, T value) {
-        prop(node_num, node_s, node_e);
+    void update(int k, int node_s, int node_e, int req_s, int req_e, T add) {
+        prop(k, node_s, node_e);
         if (req_e < node_s || node_e < req_s)
             return;
         if (req_s <= node_s && node_e <= req_e) {
-            lazy[node_num] += value;
-            prop(node_num, node_s, node_e);
+            lazy[k] += add;
+            prop(k, node_s, node_e);
             return;
         }
         int node_m = (node_s + node_e) / 2;
-        update(node_num * 2, node_s, node_m, req_s, req_e, value);
-        update(node_num * 2 + 1, node_m + 1, node_e, req_s, req_e, value);
-        node[node_num] = node[node_num * 2] + node[node_num * 2 + 1];
+        update(k * 2, node_s, node_m, req_s, req_e, add);
+        update(k * 2 + 1, node_m + 1, node_e, req_s, req_e, add);
+        node[k] = node[k * 2] + node[k * 2 + 1];
     }
 public:
     SegTree(int MIN, int MAX) : MIN(MIN), MAX(MAX) {
@@ -58,7 +58,7 @@ public:
         init(1, MIN, MAX, data);
     }
     T query(int start, int end) { return query(1, MIN, MAX, start, end); }
-    void update(int start, int end, T value) { update(1, MIN, MAX, start, end, value); }
+    void update(int start, int end, T add) { update(1, MIN, MAX, start, end, add); }
 };
 
 int main() {
