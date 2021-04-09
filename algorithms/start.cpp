a567f288b1b18a75da31653812ffd261e82b7c0b
Choi Yeonung <yonsweng@gmail.com>
2021-04-10 00:24:03 +0900

    Change start code

--- algorithms/start.cpp
+++ algorithms/start.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main() {
-    freopen("input/1.in", "r", stdin);
+    // freopen("input/1.in", "r", stdin);
+    // freopen("output/1.out", "r", stdout);
+
     ios::sync_with_stdio(false), cin.tie(nullptr);
 
 
-
-    return 0;
 }
