aab2af9edde29492c1c4e57270c5d0571da67ce2
Choi Yeonung <yonsweng@gmail.com>
2021-06-22 10:57:59 +0900

    Solve Codeforces Round 727 D

--- codeforces/1539/d.py
+++ codeforces/1539/d.py
@@ -6,18 +6,19 @@ for _ in range(n):
 
 ab = sorted(ab, key=lambda c: c[1])
 
+total = sum([c[0] for c in ab])
 answer = 0
 cnt = 0
 i = 0
 j = n - 1
-while i < n:
+while cnt < total:
     if cnt >= ab[i][1]:
         cnt += ab[i][0]
         answer += ab[i][0]
         ab[i][0] = 0
         i += 1
     else:
-        q = min(cnt - ab[i][1], ab[j][0])
+        q = min(ab[i][1] - cnt, ab[j][0])
         cnt += q
         answer += q * 2
         ab[j][0] -= q
