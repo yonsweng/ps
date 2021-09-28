b1336a5ecf304704ad87d4dfd2a403d6e8566e6a
Choi Yeonung <yonsweng@gmail.com>
2021-09-28 23:18:04 +0900

    Add read_input function to start.py

--- algorithms/start.py
+++ algorithms/start.py
@@ -1,5 +1,17 @@
+from sys import stdin
+
+
+def read_input():
+    a = stdin.readline()                          # read (a line + '\n') as an str.
+    b = int(stdin.readline())                     # read an integer.
+    c, d = stdin.readline().split()               # read words of a line.
+    e, f = map(int, stdin.readline().split())     # read two integers of a line.
+    g = list(map(int, stdin.readline().split()))  # read several integers of a line.
+    return a, b, c, d, e, f, g
+
+
 def main():
-    
+    a, b, c, d, e, f, g = read_input()
 
 
 if __name__ == '__main__':
