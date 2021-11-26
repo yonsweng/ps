b6e8cd87ca15306b6c7ff4d2d375408cd2db7a11
Choi Yeonung <yonsweng@gmail.com>
2021-11-26 19:43:09 +0900

    Edit codeforces.py

--- algorithms/codeforces.py
+++ algorithms/codeforces.py
@@ -4,20 +4,18 @@ from sys import stdin
 def read_input():
     a = stdin.readline()                          # read (a line + '\n') as an str.
     b = int(stdin.readline())                     # read an integer.
-    c, d = stdin.readline().split()               # read words of a line.
-    e, f = map(int, stdin.readline().split())     # read two integers of a line.
-    g = list(map(int, stdin.readline().split()))  # read several integers of a line.
-    return a, b, c, d, e, f, g
+    c, d = map(int, stdin.readline().split())     # read two integers of a line.
+    e = list(map(int, stdin.readline().split()))  # read several integers of a line.
+    return a, b, c, d, e
 
 
-def solve(a, b, c, d, e, f, g):
+def solve(a, b, c, d, e):
     answer = 0
     return answer
 
 
 def main():
     t = int(stdin.readline())
-
     for _ in range(t):
         input = read_input()
         answer = solve(*input)
