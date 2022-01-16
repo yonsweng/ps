84a558da5fa18e1b61b6247722b11b40d171f89c
Choi Yeonung <yonsweng@gmail.com>
2022-01-16 23:24:22 +0900

    Update codeforces.py

--- algorithms/codeforces.py
+++ algorithms/codeforces.py
@@ -1,23 +1,10 @@
 from sys import stdin
 
 
-def read_input():
-    n = int(stdin.readline())
-    a = list(map(int, stdin.readline().split()))
-    return n, a
-
-
-def solve(n, a):
-    answer = 0
-    return answer
-
-
 def main():
     t = int(stdin.readline())
     for _ in range(t):
-        input = read_input()
-        answer = solve(*input)
-        print(answer)
+        pass
 
 
 if __name__ == '__main__':
