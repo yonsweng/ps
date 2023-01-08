b0e5aea42aa9bfce8ca84fb9d8128091560b42e2
Choi Yeonung <yonsweng@gmail.com>
2023-01-08 17:28:58 +0900

    Edit start.py

--- algorithms/start.py
+++ algorithms/start.py
@@ -1,25 +1,9 @@
 from sys import stdin
 
 
-def read_input():
-    a = stdin.readline()                          # read (a line + '\n') as an str.
-    b = int(stdin.readline())                     # read an integer.
-    c, d = stdin.readline().split()               # read words of a line.
-    e, f = map(int, stdin.readline().split())     # read two integers of a line.
-    g = list(map(int, stdin.readline().split()))  # read several integers of a line.
-    return a, b, c, d, e, f, g
-
-
-def solve(a, b, c, d, e, f, g):
-    answer = 0
-    return answer
-
-
 def main():
-    input = read_input()
-    answer = solve(*input)
-    print(answer)
+    n = stdin.readline()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
