1f93ad122c1cc45f338abd3c0307b0af6ae4f45b
Choi Yeonung <yonsweng@gmail.com>
2023-01-15 19:17:23 +0900

    update hello-world.go

--- algorithms/hello-world.go
+++ algorithms/hello-world.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	var name string
+	in := bufio.NewReader(os.Stdin)
+	fmt.Fscan(in, &name)
+	fmt.Printf("Hello, %s!", name)
 }
