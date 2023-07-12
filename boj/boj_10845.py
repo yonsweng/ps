f9841f8adc5354508247a28fb41a04ad9988d687
Choi Yeonung <yonsweng@gmail.com>
2023-07-13 01:30:16 +0900

    Solve boj 10845

---
+++ boj/boj_10845.py
@@ -0,0 +1,31 @@
+from sys import stdin
+from collections import deque
+
+q = deque()
+n = int(stdin.readline())
+for _ in range(n):
+    cmd = stdin.readline().split()
+    if cmd[0] == "push":
+        q.append(cmd[1])
+    elif cmd[0] == "pop":
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == "size":
+        print(len(q))
+    elif cmd[0] == "empty":
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == "front":
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif cmd[0] == "back":
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
