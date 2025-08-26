65083255b9afd15c8cc4a2ff29b3a78b1a975966
Choi Yeonung <yonsweng@gmail.com>
2025-08-27 00:26:20 +0900

    Add solution for boj 14358

---
+++ boj/boj_14358.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+
+def solve():
+    T = int(stdin.readline().strip())
+    for t in range(T):
+        N, K = map(int, stdin.readline().strip().split())
+        P = list(map(float, stdin.readline().strip().split()))
+        P.sort()
+
+        answer = 0
+
+        for k in range(K + 1):
+            comb = P[:k] + P[N - (K - k):]
+            dp = [[0] * (K // 2 + 1) for _ in range(K + 1)]
+            dp[0][0] = 1
+
+            for i in range(1, K + 1):
+                for j in range(min(i, K // 2) + 1):
+                    dp[i][j] = dp[i - 1][j] * (1 - comb[i - 1])
+                    if j > 0:
+                        dp[i][j] += dp[i - 1][j - 1] * comb[i - 1]
+
+            answer = max(answer, dp[K][K // 2])
+
+        print("Case #{}: {}".format(t + 1, answer), flush=False)
+
+
+if __name__ == "__main__":
+    solve()
