b16ea4bd30f43680412fd93a2e74af30c431100d
Choi Yeonung <yonsweng@gmail.com>
2026-02-01 15:51:35 +0900

    Add solution for boj 5875

--- boj/boj_5875.py
+++ boj/boj_5875.py
@@ -4,41 +4,31 @@ from sys import stdin
 def solve():
     p = stdin.readline().strip()
 
-    if len(p) == 1:
-        print(0)
-        return
-
-    if p[0] == ")" or p[-1] == "(":
-        print(1)
-        return
-
     left_count = p.count("(")
     right_count = p.count(")")
-
-    if left_count == right_count:
-        print(0)
-        return
 
     result = 0
 
     if left_count == right_count + 2:
         left_count, right_count = 0, 0
+        for char in p[::-1]:
+            if char == ")":
+                right_count += 1
+            else:
+                left_count += 1
+                if left_count > right_count:
+                    result = left_count
+                    break
+    elif right_count == left_count + 2:
+        left_count, right_count = 0, 0
         for char in p:
             if char == "(":
                 left_count += 1
-                if left_count > right_count + 1:
-                    result += 1
             else:
                 right_count += 1
-    elif right_count == left_count + 2:
-        left_count, right_count = 0, 0
-        for char in p[::-1]:
-            if char == "(":
-                left_count += 1
-            else:
-                right_count += 1
-                if right_count > left_count + 1:
-                    result += 1
+                if right_count > left_count:
+                    result = right_count
+                    break
 
     print(result)
 
